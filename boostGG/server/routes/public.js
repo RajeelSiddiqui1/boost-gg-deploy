ff3b9bdd727d4b83dd889840e2674533bdeb7a4b
Rajeel Siddiqui github <159783237+RajeelSiddiqui1@users.noreply.github.com>
2026-04-08 01:26:39 +0500

    Remove boostGG folder

--- boostGG/server/routes/public.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getPublicSettings } = require('../controllers/adminController');
-
-const router = express.Router();
-
-router.get('/settings', getPublicSettings);
-
-module.exports = router;
--- boostGG/server/routes/uploads.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { uploadMultiple } = require('../controllers/uploadController');
-const { protect, authorize } = require('../middleware/auth');
-const upload = require('../middleware/upload');
-
-router.post('/multiple', protect, authorize('admin'), upload.array('files', 10), uploadMultiple);
-
-module.exports = router;
